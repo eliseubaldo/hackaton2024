e6feaa311c49e531fe0f8d9c69059d9e772b94b5
Samuel Comeau <24446727+sam-com@users.noreply.github.com>
2024-03-20 12:56:12 -0400

    Update: bundle and publish to artifactory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
-	clearScreen: false,
-})
+  plugins: [],
+  build: {
+    lib: { entry: resolve(__dirname, "src/main.js"), name: "hackathon-2024" },
+  },
+  server: { host: "0.0.0.0", port: 8000 },
+  clearScreen: false,
+});
